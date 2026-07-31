931c30292629ba9031f599847d993ebb205ea9fc
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-07-31 17:11:26 +1000

    Timer UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
+
 const App = () => {
 
-    const inputRef = useRef(null);
-    // console.log(inputRef);
+    const timerRef = useRef(null);
 
-    const submit = () => {
-        // console.log(inputRef.current);
-        // inputRef.current.focus();
-        // inputRef.current.blur();
-        console.log(inputRef.current.value);
-        inputRef.current.style.backgroundColor = "red";
-        inputRef.current.style.color = "white";
-        inputRef.current.setAttribute('placeholder', 'Enter somethingggg...')
-    }
+    const [time, setTime] = useState(0);
+
+    const [isRunning, setIsRunning] = useState(false);
 
     return ( 
         <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
-            <h2 className="text-2xl font-bold mb-4">useRef Example</h2>
-            <input 
-                className="w-full p-2 border rounded-lg" 
-                type="text" 
-                placeholder="Type something..."
-                ref={inputRef}
-                />
-            <button onClick={submit} className="mt-2 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-600 transition">
-                Submit
+            <h2 className="text-4xl font-semibold mt-4">
+                ⏳ Timer: {time}
+            </h2>
+            <button className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer">
+                { isRunning ? 'Pause': 'Start' }
             </button>
         </div>
      );
