da662abf9f272305494648b778951a6687e38283
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-03 22:11:49 +1000

    Auto focus on start button

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useRef } from "react";
+
 const TimerControls = ({isRunning, onToggle, onReset}) => {
+
+    const startButtonRef = useRef(null);
+
+    useEffect(() => {
+        if(startButtonRef.current){
+            startButtonRef.current.focus();
+        }
+    }, []);
+
     return ( 
         <>
-            <button onClick={onToggle} className="mt-3 mr-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer">
+            <button ref={startButtonRef} onClick={onToggle} className="mt-3 mr-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer">
                 { isRunning ? 'Pause': 'Start' }
             </button>
             <button onClick={onReset} className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 cursor-pointer">
